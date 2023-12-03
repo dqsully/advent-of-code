e2e42f8c46b44a18d0a25517caf9427a55500cba
Dakota Sullivan <djqballer@outlook.com>
2023-12-02 18:23:34 -0700

    Add table tests for day one helper functions

--- 2023/01/rust/src/part_1.rs
+++ 2023/01/rust/src/part_1.rs
@@ -39,4 +39,28 @@ mod tests {
 
         assert_eq!(output.unwrap(), "142");
     }
+
+    macro_rules! number_for_line_test_happy {
+        ($suite:ident, $($name: ident: $input:expr => $expected:expr,)*) => {
+            mod $suite {
+                use super::*;
+
+                $(
+                    #[test]
+                    fn $name() {
+                        let output = number_for_line($input);
+
+                        assert_eq!(output.unwrap(), $expected);
+                    }
+                )*
+            }
+        }
+    }
+
+    number_for_line_test_happy!(number_for_line_tests,
+        example_line_1: "1abc2" => 12,
+        example_line_2: "pqr3stu8vwx" => 38,
+        example_line_3: "a1b2c3d4e5f" => 15,
+        example_line_4: "treb7uchet" => 77,
+    );
 }
--- 2023/01/rust/src/part_2.rs
+++ 2023/01/rust/src/part_2.rs
@@ -57,4 +57,73 @@ mod tests {
 
         assert_eq!(output.unwrap(), "281");
     }
+
+    macro_rules! number_for_line_test_happy {
+        ($suite:ident, $($name: ident: $input:expr => $expected:expr,)*) => {
+            mod $suite {
+                use super::*;
+
+                $(
+                    #[test]
+                    fn $name() {
+                        let output = number_for_line($input);
+
+                        assert_eq!(output.unwrap(), $expected);
+                    }
+                )*
+            }
+        }
+    }
+
+    number_for_line_test_happy!(number_for_line_tests,
+        example_line_1: "two1nine" => 29,
+        example_line_2: "eightwothree" => 83,
+        example_line_3: "abcone2threexyz" => 13,
+        example_line_4: "xtwone3four" => 24,
+        example_line_5: "4nineeightseven2" => 42,
+        example_line_6: "zoneight234" => 14,
+        example_line_7: "7pqrstsixteen" => 76,
+
+        overlap_oneight: "oneight" => 18,
+        overlap_twone: "twone" => 21,
+        overlap_threeight: "threeight" => 38,
+        overlap_fiveight: "fiveight" => 58,
+        overlap_sevenine: "sevenine" => 79,
+        overlap_eightwo: "eightwo" => 82,
+        overlap_eighthree: "eighthree" => 83,
+        overlap_nineight: "nineight" => 98,
+        overlap_twoneight: "twoneight" => 28,
+        overlap_eightwoneight: "eightwoneight" => 88,
+    );
+
+    macro_rules! digit_from_str_test {
+        ($suite:ident, $($name: ident: $input:expr => $expected:expr,)*) => {
+            mod $suite {
+                use super::*;
+
+                $(
+                    #[test]
+                    fn $name() {
+                        let output = digit_from_str($input);
+
+                        assert_eq!(output, $expected);
+                    }
+                )*
+            }
+        }
+    }
+
+    digit_from_str_test!(digit_from_str_tests,
+        one: "one" => Some(1),
+        two: "two" => Some(2),
+        three: "three" => Some(3),
+        four: "four" => Some(4),
+        five: "five" => Some(5),
+        six: "six" => Some(6),
+        seven: "seven" => Some(7),
+        eight: "eight" => Some(8),
+        nine: "nine" => Some(9),
+
+        must_be_at_start: "jibberishone" => None,
+    );
 }
