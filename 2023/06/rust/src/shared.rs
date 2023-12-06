c108e18c9252f39f665fca872535e65bb6e636d0
Dakota Sullivan <djqballer@outlook.com>
2023-12-06 08:57:49 -0700

    Undo floor/ceil offset change

--- 2023/06/rust/src/shared.rs
+++ 2023/06/rust/src/shared.rs
@@ -1,8 +1,8 @@
 pub fn valid_inputs_for_problem(time: f64, distance: f64) -> i32 {
     let o = (time * time / 4.0 - distance).sqrt();
 
-    let min = (-o + time / 2.0).floor() as i32;
-    let max = (o + time / 2.0).ceil() as i32 - 1;
+    let min = (-o + time / 2.0).floor() as i32 + 1;
+    let max = (o + time / 2.0).ceil() as i32;
 
     max - min
 }
