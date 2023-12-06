a3341a174be104d5a570c437b4975eaf689e38c6
Dakota Sullivan <djqballer@outlook.com>
2023-12-05 22:25:43 -0700

    Solve day 6

---
+++ 2023/06/rust/src/part_1.rs
@@ -0,0 +1,45 @@
+use crate::error::Error;
+
+pub fn run(input: &str) -> Result<String, Error> {
+    let (times, distances) = input.split_once('\n').unwrap();
+
+    let times = times.split_once(':').unwrap().1.trim()
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<f64>().unwrap())
+        .collect::<Vec<_>>();
+    let distances = distances.split_once(':').unwrap().1.trim()
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<f64>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut options = 1;
+
+    for (time, distance) in times.iter().copied().zip(distances.iter().copied()) {
+        let o = (time*time/4.0 - distance).sqrt();
+
+        let min = (-o + time/2.0).floor() as i32 + 1;
+        let max = (o + time/2.0).ceil() as i32;
+
+        options *= max - min;
+    }
+
+    Ok(options.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = r"Time:      7  15   30
+        Distance:  9  40  200
+        ";
+
+        let output = run(input);
+
+        assert_eq!(output, Ok("288".to_owned()));
+    }
+}
---
+++ 2023/06/rust/src/part_2.rs
@@ -0,0 +1,40 @@
+use crate::error::Error;
+
+pub fn run(input: &str) -> Result<String, Error> {
+    let (times, distances) = input.split_once('\n').unwrap();
+
+    let time = times.split_once(':').unwrap().1.trim()
+        .chars()
+        .filter(|&c| c != ' ')
+        .collect::<String>()
+        .parse::<f64>().unwrap();
+    let distance = distances.split_once(':').unwrap().1.trim()
+        .chars()
+        .filter(|&c| c != ' ')
+        .collect::<String>()
+        .parse::<f64>().unwrap();
+
+
+    let o = (time*time/4.0 - distance).sqrt();
+
+    let min = (-o + time/2.0).floor() as i32 + 1;
+    let max = (o + time/2.0).ceil() as i32;
+
+    Ok((max - min).to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = r"Time:      7  15   30
+        Distance:  9  40  200
+        ";
+
+        let output = run(input);
+
+        assert_eq!(output, Ok("288".to_owned()));
+    }
+}
