259db556255fd7107d0e135a60fe33208c63d62a
Dakota Sullivan <djqballer@outlook.com>
2023-12-06 22:39:26 -0700

    Solve day 7

---
+++ 2023/07/rust/src/main.rs
@@ -0,0 +1,15 @@
+// #![deny(clippy::all, clippy::pedantic)]
+use aoc_helpers::answer::AocAnswer;
+use std::fs;
+
+mod error;
+mod part_1;
+mod part_2;
+mod shared;
+
+fn main() {
+    let input = fs::read_to_string("../input.txt").unwrap();
+
+    part_1::run(&input).print_aoc_answer(1);
+    part_2::run(&input).print_aoc_answer(2);
+}
---
+++ 2023/07/rust/src/part_1.rs
@@ -0,0 +1,151 @@
+use std::{collections::HashMap, cmp::Ordering};
+
+use crate::error::Error;
+
+pub fn run(input: &str) -> Result<String, Error> {
+    let mut hands: Vec<(Hand, u32)> = input.lines()
+        .map(|line| {
+            let (hand, bid) = line.split_once(' ').unwrap();
+
+            (Hand::parse(hand), bid.parse().unwrap())
+        })
+        .collect();
+
+    hands.sort_unstable_by_key(|h| h.0.clone());
+
+    let mut total_winnings = 0;
+
+    for (i, hand) in hands.into_iter().enumerate() {
+        total_winnings += (i + 1) * hand.1 as usize;
+    }
+
+    Ok(total_winnings.to_string())
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy)]
+enum HandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+enum Card {
+    C2,
+    C3,
+    C4,
+    C5,
+    C6,
+    C7,
+    C8,
+    C9,
+    CT,
+    CJ,
+    CQ,
+    CK,
+    CA,
+}
+
+impl From<char> for Card {
+    fn from(value: char) -> Self {
+        match value {
+            '2' => Card::C2,
+            '3' => Card::C3,
+            '4' => Card::C4,
+            '5' => Card::C5,
+            '6' => Card::C6,
+            '7' => Card::C7,
+            '8' => Card::C8,
+            '9' => Card::C9,
+            'T' => Card::CT,
+            'J' => Card::CJ,
+            'Q' => Card::CQ,
+            'K' => Card::CK,
+            'A' => Card::CA,
+            _ => panic!("not a card: {value:?}")
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct Hand<'a>(&'a str, HandType);
+
+impl<'a> Hand<'a> {
+    fn parse(s: &str) -> Hand {
+        let mut chars: HashMap<char, u32> = HashMap::new();
+
+        for ch in s.chars() {
+            chars.entry(ch)
+                .and_modify(|n| *n += 1)
+                .or_insert(1);
+        }
+
+        let mut chars = chars.into_iter().collect::<Vec<_>>();
+        chars.sort_unstable_by_key(|ch| ch.1);
+        chars.reverse();
+
+        let hand_type = match chars.len() {
+            1 => HandType::FiveOfAKind,
+            2 => match chars[0].1 {
+                4 => HandType::FourOfAKind,
+                3 => HandType::FullHouse,
+                _ => panic!("2 variants: {s:?}"),
+            },
+            3 => match chars[0].1 {
+                3 => HandType::ThreeOfAKind,
+                2 => HandType::TwoPair,
+                _ => panic!("3 variants: {s:?}"),
+            },
+            4 => HandType::OnePair,
+            5 => HandType::HighCard,
+            _ => panic!("more than 5 variants: {s:?}"),
+        };
+
+        Hand(s, hand_type)
+    }
+}
+
+impl<'a> std::cmp::Ord for Hand<'a> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.1.cmp(&other.1).then_with(|| {
+            for (s, o) in self.0.chars().zip(other.0.chars()) {
+                let cmp = Card::from(s).cmp(&Card::from(o));
+
+                if cmp != Ordering::Equal {
+                    return cmp;
+                }
+            }
+
+            Ordering::Equal
+        })
+    }
+}
+
+impl<'a> std::cmp::PartialOrd for Hand<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = r"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+";
+
+        let output = run(input);
+
+        assert_eq!(output, Ok("6440".to_owned()));
+    }
+}
