9444808c9f5199d7cc1c45e53b780c31d76bd173
Dakota Sullivan <djqballer@outlook.com>
2023-12-10 15:30:33 -0700

    Fix day 6 lints

--- 2023/06/rust/src/shared.rs
+++ 2023/06/rust/src/shared.rs
@@ -1,7 +1,9 @@
 pub fn valid_inputs_for_problem(time: f64, distance: f64) -> i32 {
     let o = (time * time / 4.0 - distance).sqrt();
 
+    #[allow(clippy::cast_possible_truncation)]
     let min = (-o + time / 2.0).floor() as i32 + 1;
+    #[allow(clippy::cast_possible_truncation)]
     let max = (o + time / 2.0).ceil() as i32;
 
     max - min
